00979d5cc21018b993cc6f675ca0811d6eaf09f9
Abhishek Gade <abhishekgade790@gmail.com>
2025-02-20 15:53:05 +0530

    Episode 01 Ended

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom/client"; 
 const parent = React.createElement("div",{id:"parent"},
     React.createElement("div",{id:"child1"},
-        [React.createElement("h1",{},"i am h1 tag"),React.createElement("h1",{},"heading two")]
+        [React.createElement("h1",{},"i am h2 tag by abhi"),React.createElement("h1",{},"heading 2")]
     )
 )
 const root = ReactDOM.createRoot(document.getElementById("root"));
